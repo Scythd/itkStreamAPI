f41e0590f6d272a225eda502aa5be68bbdbd72c7
Никита Мокляк <nikita.dezertir@gmail.com>
2025-10-25 19:58:08 +0400

    #00002 second stream API task

--- src/main/java/com/moklyak/Main.java
+++ src/main/java/com/moklyak/Main.java
@@ -1,13 +1,13 @@
 package com.moklyak;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+
+        // First task
         List<Order> orders = List.of(
                 new Order("Laptop", 1200.0),
                 new Order("Smartphone", 800.0),
@@ -44,5 +44,23 @@ public class Main {
 
         System.out.println(groupedAndTotalSortedTop3);
 
+
+        // second task
+        List<Student> students = Arrays.asList(
+                new Student("Student1", Map.of("Math", 90, "Physics", 85)),
+                new Student("Student2", Map.of("Math", 95, "Physics", 88)),
+                new Student("Student3", Map.of("Math", 88, "Chemistry", 92)),
+                new Student("Student4", Map.of("Physics", 78, "Chemistry", 85))
+        );
+
+        Map<String, Double> result = students.stream()
+                .parallel()
+                .flatMap(x -> x.getGrades().entrySet().stream())
+                .collect(Collectors.groupingBy(Map.Entry::getKey))
+                .entrySet().stream()
+                .map(x -> new AbstractMap.SimpleEntry<>(x.getKey(), x.getValue().stream().mapToInt(Map.Entry::getValue).average().orElseThrow()))
+                .collect(Collectors.toMap(Map.Entry::getKey, AbstractMap.SimpleEntry::getValue));
+
+        System.out.println(result);
     }
 }
