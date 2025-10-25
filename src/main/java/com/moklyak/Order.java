ac48fc67f17d11ac66442deb488ae45b9586a03a
Никита Мокляк <nikita.dezertir@gmail.com>
2025-10-25 19:35:09 +0400

    #00001 first stream API task

---
+++ src/main/java/com/moklyak/Order.java
@@ -0,0 +1,29 @@
+package com.moklyak;
+
+import java.util.List;
+
+class Order {
+    private String product;
+    private double cost;
+
+    public Order(String product, double cost) {
+        this.product = product;
+        this.cost = cost;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "product='" + product + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+}
